2b543b97e8e2eb8a9bd43160b71a47b4ab7321be
MFactor1 <mingsqu@gmail.com>
2024-12-12 00:23:41 -0500

    change skill ordering

--- src/App.js
+++ src/App.js
@@ -372,7 +372,7 @@ function Skills() {
           <b><i>Frameworks/Libraries:</i></b> React.JS, Node.JS, gevent, kivy, OpenCV, spaCy, JUnit, unittest.
         </p>
         <p className='skillItem'>
-          <b><i>Tools:</i></b> git, Linux, Cron, Docker, Jenkins, Jira, Azure Blob Storage, GCS, Amazon S3, rpm, dpkg, Sonarqube.
+          <b><i>Tools:</i></b> git, Linux, Docker, Jenkins, Jira, Azure Blob Storage, GCS, Amazon S3, rpm, dpkg, cron, Sonarqube.
         </p>
       </div>
     </>
