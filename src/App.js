ebdf2cf48d552df873b876bae2f7d0ca21df7dee
MFactor1 <mingsqu@gmail.com>
2024-12-14 18:33:00 -0500

    Update skills page

--- src/App.js
+++ src/App.js
@@ -379,7 +379,7 @@ function Skills() {
           <b><i>Frameworks/Libraries:</i></b> React.JS, Node.JS, gevent, kivy, OpenCV, spaCy, JUnit, unittest.
         </p>
         <p className='skillItem'>
-          <b><i>Tools:</i></b> git, Linux, Docker, Jenkins, Jira, Azure Blob Storage, GCS, Amazon S3, Make, rpm, dpkg, cron, Sonarqube.
+          <b><i>Tools:</i></b> git, Linux, Docker, Jenkins, Jira, MS Office, Azure Blob Storage, GCS, Amazon S3, Make, rpm, dpkg, UML, Sonarqube.
         </p>
       </div>
     </>
