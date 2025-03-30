afcf338670ba46889328519f035ee1dc07550bf5
MFactor1 <mingsqu@gmail.com>
2025-03-30 00:29:52 -0400

    Fix home pg Agora link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
         <ScrollFade><p>Hi, thanks for stopping by!</p></ScrollFade>
         <ScrollFade><i>Where I'm at:</i></ScrollFade>
         <ScrollFade>Pursuing Software Engineering at McMaster University</ScrollFade>
-        <ScrollFade>Working to grow the Agora community (<a href='https://lu.ma/theagora'>https://lu.ma/theagora</a>)</ScrollFade>
+        <ScrollFade>Working to grow the Agora community (<a href='https://lu.ma/theagora' target="_blank">https://lu.ma/theagora</a>)</ScrollFade>
         <ScrollFade>Pushing my limits on a road bike</ScrollFade>
         <ScrollFade>Searching for where the snow is fresh</ScrollFade>
 			</div>
